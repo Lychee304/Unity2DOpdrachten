7b47c4d82889ed831d74574b98324a2769047335
Lychee304 <2173013@talnet.nl>
2025-12-05 11:47:53 +0100

    coin spawner multiple
    im rich
    
    - the scene probably

---
+++ Assets/Scripts/CoinSpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _coinPrefab;
+
+    [SerializeField] private GameObject _coin;
+
+    [SerializeField] private List<Transform> _coinSpawnLocations = new List<Transform>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+
+        foreach(Transform location in _coinSpawnLocations)
+        {
+            Instantiate(_coin, transform.position, transform.rotation);
+
+            Instantiate(_coinPrefab, location.position, location.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
